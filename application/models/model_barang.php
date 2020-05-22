779e9aff7e6d80fb0b20f612c149746d98ef5d09
listawulandari <62386316+listawulandari@users.noreply.github.com>
2020-05-22 14:13:18 +0700

    Add files via upload

---
+++ application/models/model_barang.php
@@ -0,0 +1,60 @@
+<?php
+
+class Model_barang extends CI_Model{
+	public function tampil_data(){
+		return $this->db->get('produk');
+	}
+
+	public function tambah_barang($data,$table){
+		$this->db->insert($table,$data);
+	}
+
+	public function edit_barang($where,$table){
+		return $this->db->get_where($table, $where);
+	}
+
+	public function update_data($where,$data,$table)
+	{
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}
+
+	public function hapus_data($where,$table){
+		$this->db->where($where);
+		$this->db->delete($table);
+	}
+
+	public function find($id)
+	{
+		$result = $this->db->where('id_barang', $id)
+							->limit(1)
+							->get('produk');
+		if($result->num_rows() > 0){
+			return $result->row();
+		}else{
+			return array();
+		}
+	}
+
+	public function detail_barang($id_barang)
+	{
+		$result = $this->db->where('id_barang',$id_barang)->get('produk');
+		if($result->num_rows() > 0){
+			return $result->result();
+		}else {
+			return false;
+		}
+	}
+
+	public function get_keyword($keyword){
+		$this->db->select('*');
+		$this->db->from('produk');
+		$this->db->like('nama_barang', $keyword);
+		$this->db->or_like('deskripsi', $keyword);
+		$this->db->or_like('harga', $keyword);
+		$this->db->or_like('kategori', $keyword);
+		$this->db->or_like('stok', $keyword);
+		return $this->db->get()->result();
+
+	}
+}
---
+++ toko_online(1).sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2020 at 03:30 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(60) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice`
+--
+
+INSERT INTO `invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(12, 'oki', 'jakarta', '2020-04-04 12:59:00', '2020-04-05 12:59:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `id_invoice`, `id_barang`, `nama_barang`, `jumlah`, `harga`, `pilihan`) VALUES
+(12, 1, 1235, 'kecap', 2, 23000, '');
+
+--
+-- Triggers `pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` BEFORE INSERT ON `pesanan` FOR EACH ROW BEGIN 
+	UPDATE barang SET stok = stok-NEW.jumlah
+    WHERE id_barang = NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `deskripsi` varchar(150) NOT NULL,
+  `harga` int(20) DEFAULT NULL,
+  `kategori` varchar(20) DEFAULT NULL,
+  `stok` int(20) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_barang`, `nama_barang`, `deskripsi`, `harga`, `kategori`, `stok`, `gambar`) VALUES
+('1235', 'kecap', 'kecap asin', 23000, 'makanan', 10, 'kecap.jpg'),
+('1236', 'Beras', 'Beras', 60000, 'makanan', 12, 'beras.png'),
+('1237', 'Sapu', 'Sapu', 19000, 'rumah', 9, 'sapu.jpg'),
+('1238', 'pel', 'lap pel dan tempat airnya', 92000, 'rumah', 10, 'pel.jpg'),
+('1239', 'gula', 'gula tebu asli', 24000, 'makanan', 6, 'gula.jpg'),
+('1240', 'rinso', 'rinso bubuk ', 19000, 'rumah', 5, 'rinso.jpg'),
+('1241', 'saos', 'saos pedas', 11000, 'makanan', 7, 'saos.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', 'a', 1),
+(2, 'user', 'user', 'a', 2),
+(3, 'a', 'a', 'a', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
